b8c4e5bc927e8e71263bb8bc442afd01eb6c1806
Diego Farias <diego.farias.fernandes@gmail.com>
2023-11-15 18:26:39 -0300

    Create up-filmes.py

---
+++ up-filmes.py
@@ -0,0 +1,10 @@
+"""
+  Preciso de dados dos filmes seguintes, para completar o que já coletamos.
+A fonte gratuita que você pode usar é BoxOfficeMojo.com.
+
+https://www.dropbox.com/scl/fi/pg10fb4ue9uzgj3sg1a4b/data-nov-14-for-upwork.xlsx?rlkey=upqxfhwgm2vrnhff9i28abllg&dl=0
+
+Use a lista de filmes acima como ponto de partida, mas preencha mais filmes se você os encontra. A questão é não parar apenas nos “100 maiores bilheterias”, etc., pois precisamos de TODOS os filmes desta fonte de dados. Observe nos dados acima que algumas datas de lançamento e outros detalhes são imprecisos ou suspeitos, como orçamentos de US$ 1.000 para filmes importantes. Corrija isso e use apenas o anexo como guia. Concentre-se principalmente nos títulos da planilha acima.
+
+Tudo que preciso é de uma planilha com todos os detalhes de cada filme daquele site. Não há restrições sobre como você coleta isso ou qual linguagem ou método você usa, mas em anexo está o script python usado pelo freelancer anterior (se for útil).
+"""
